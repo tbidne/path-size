3298947914226950597e907b01bc4edc7fb97715
Tommy Bidne <tbidne@protonmail.com>
2022-12-04 08:14:01 +1300

    Swap flake-utils for flake-parts

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
       flake = false;
     };
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -39,46 +40,51 @@
     , bounds
     , byte-types
     , flake-compat
+    , flake-parts
     , flake-utils
     , nixpkgs
     , self
     }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      buildTools = c: with c; [
-        cabal-install
-        pkgs.gnumake
-        pkgs.zlib
-      ];
-      # add tools like hlint, ormolu, ghcid here if you want them
-      # on the PATH
-      devTools = c: with c; [
-        ghcid
-        haskell-language-server
-      ];
-      ghc-version = "ghc924";
-      compiler = pkgs.haskell.packages."${ghc-version}";
-      mkPkg = returnShellEnv: withDevTools:
-        compiler.developPackage {
-          inherit returnShellEnv;
-          name = "path-size";
-          root = ./.;
-          modifier = drv:
-            pkgs.haskell.lib.addBuildTools drv
-              (buildTools compiler ++
-                (if withDevTools then devTools compiler else [ ]));
-          overrides = final: prev: with compiler; {
-            algebra-simple =
-              final.callCabal2nix "algebra-simple" algebra-simple { };
-            bounds = final.callCabal2nix "bounds" bounds { };
-            byte-types = final.callCabal2nix "byte-types" byte-types { };
-          };
+    flake-parts.lib.mkFlake { inherit self; } {
+      perSystem = { pkgs, ... }:
+        let
+          buildTools = c: with c; [
+            cabal-install
+            pkgs.gnumake
+            pkgs.zlib
+          ];
+          # add tools like hlint, ormolu, ghcid here if you want them
+          # on the PATH
+          devTools = c: with c; [
+            ghcid
+            haskell-language-server
+          ];
+          ghc-version = "ghc924";
+          compiler = pkgs.haskell.packages."${ghc-version}";
+          mkPkg = returnShellEnv: withDevTools:
+            compiler.developPackage {
+              inherit returnShellEnv;
+              name = "path-size";
+              root = ./.;
+              modifier = drv:
+                pkgs.haskell.lib.addBuildTools drv
+                  (buildTools compiler ++
+                    (if withDevTools then devTools compiler else [ ]));
+              overrides = final: prev: with compiler; {
+                algebra-simple =
+                  final.callCabal2nix "algebra-simple" algebra-simple { };
+                bounds = final.callCabal2nix "bounds" bounds { };
+                byte-types = final.callCabal2nix "byte-types" byte-types { };
+              };
+            };
+        in
+        {
+          packages.default = mkPkg false false;
+          devShells.default = mkPkg true true;
+          devShells.ci = mkPkg true false;
         };
-    in
-    {
-      packages.default = mkPkg false false;
-      devShells.default = mkPkg true true;
-      devShells.ci = mkPkg true false;
-    });
+      systems = [
+        "x86_64-linux"
+      ];
+    };
 }
