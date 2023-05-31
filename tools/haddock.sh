20017484f807e5cb31f106a8e53009aa296f5b2c
Tommy Bidne <tbidne@protonmail.com>
2023-06-01 11:56:10 +1200

    Fix haddock script

--- tools/haddock.sh
+++ tools/haddock.sh
@@ -9,4 +9,4 @@ mkdir -p docs/
 # shellcheck disable=SC2038
 find docs/ -type f | xargs -I % sh -c "rm -r %"
 
-cp -r dist-newstyle/build/x86_64-linux/ghc-9.4.4/path-size-0.1/doc/html/path-size/* docs/
+cp -r dist-newstyle/build/x86_64-linux/ghc-9.4.4/path-size-0.1/opt/doc/html/path-size/* docs/
