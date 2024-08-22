4fa18eb708f8464977f89ff9ed26639fe393f1c6
Tommy Bidne <tbidne@protonmail.com>
2024-08-22 15:02:08 +1200

    Update profiling

--- profile/Main.hs
+++ profile/Main.hs
@@ -31,29 +31,6 @@ import PathSize.Data.Config
 import System.Environment (getArgs)
 import System.Exit (die)
 
--- Profiling doesn't show a lot of low-hanging fruit. One of the more
--- expensive operations appears to be decodeOsToFp, which we call since
--- we need to convert our OsPaths into FilePaths so that they can be used
--- with getPathType, which uses unix-compat's getSymbolicLinkStatus.
--- There are a couple options:
---
--- 1. Downgrade to FilePath.
---
---    - I really don't want to do this as it the OsPath type is much better.
---
--- 2. Somehow skip the conversion.
---
---   - This may actually be possible because the unix package offers the
---     same function in terms of PosixPath, which is what OsPath is under the
---     hood (on unix). So theoretically we could use this function directly.
---
---     The main annoyance is that getPathType comes from monad-effects, so
---     we'd need to make some changes there. This definitely seems worth doing,
---     but it's not entirely straightforward. We'd need to depend on unix
---     conditionally with CPP, and use those functions when available.
---     Otherwise use unix-compat. We'd want the type signatures to align,
---     however, so that may mean conversions from OsPath to FilePath.
-
 main :: IO ()
 main = do
   setUncaughtExceptionHandler (putStrLn . displayException)
